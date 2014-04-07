78aa3bbc66e1ae00d87f53b3756bbc930d69a2a1
Jason Gonzales <jason.gonzales23@gmail.com>
2014-04-07 00:57:27 -0700

    remove more secret stuff

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Afar::Application.config.secret_token = '9177b182e7d80af0eb5bbbf136099556e6f443457dd98c21029fa8b32c761b37058a746ec90a38cf084d8130d5d4ed33c45aeae80c6b86944edf601cca62af02'
+Afar::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+                                          ('x' * 30)
+                                        else
+                                          ENV['SECRET_TOKEN']
+                                        end
